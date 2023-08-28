9f8aecfc3ba8177f5f2b16ae6bcc7deab37f6cfd
Potassium <brainiacgamer444@gmail.com>
2023-08-28 18:28:06 +0545

    Make function to display markers on DOM

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
+// Update board array only
 const gameBoard = (() => {
     const _board = new Array(9).fill("");
     console.log(_board);
     return { _board };
 })();
 
+// Run game
 const gameFlow = (() => {
     let currentMarker;
     let toggleCircle;
+    
+    const getCurrentMaker = () => currentMarker;
     const swapTurn = () => {
-        toggleCircle = false;
-        currentMarker = toggleCircle ? "cross" : "circle";
+        toggleCircle = !toggleCircle;
+        currentMarker = toggleCircle ? "circle" : "cross";
     }
-    return {swapTurn};
+    return {getCurrentMaker, swapTurn};
 })();
 
+// Update DOM display
 const displayController = (() => {
     const squareBox = document.querySelectorAll(".square-box");
 
     const displayMarker = (event) => {
-        return event.target.classList.add("cross");
+        gameFlow.swapTurn();
+        return event.target.classList.add(gameFlow.getCurrentMaker());
     }
 
     squareBox.forEach(box => box.addEventListener("click", displayMarker));
